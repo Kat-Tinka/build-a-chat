805e9eda9e373301ad0d06cc85c3640c97f43267
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-10-14 17:40:16 +0200

    init some styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+const style ={
+  
+}
+
 function App() {
   return (
-    <div >
+    <div className=>'max-w-[728px] mx-auto text-center'>
+    <section className=>'flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative'>
+      Chat
+    </section>
       
     </div>
   );
