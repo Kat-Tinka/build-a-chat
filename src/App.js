3618deaf8c46932883bdbf4c4d0efa9e8759c717
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-10-14 17:54:19 +0200

    create App Container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+// use the style code in const style with backticks and seperate each line with a coma in Tailwind
+
 const style ={
-  
+  appContainer:`max-w-[728px] mx-auto text-center`,
+  sectionContainer:`flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
 }
 
 function App() {
   return (
-    <div className=>'max-w-[728px] mx-auto text-center'>
-    <section className=>'flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative'>
+    <div className={style.appContainer}>
+    <section className={style.sectionContainer}>
+    {/*Navbar*/}
+    {/*Chat component*/}
       Chat
     </section>
       
