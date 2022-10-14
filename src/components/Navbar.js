c47b804af57b6be6935d423aed398730b7c9da3a
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-10-14 18:19:19 +0200

    install firebase, firebase hooks, googleButton

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 // shortCommand rafce
 import React from 'react'
 
+const style = { 
+   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+   heading: `text-white text-3xl`
+}
+
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className={style.nav}>
+      <h1 className={style.heading}>Build a Chat App</h1>
+    </div>
+  ); 
 }
 
 export default Navbar
